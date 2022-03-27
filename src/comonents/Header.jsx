63fb171d27f9487fce32a2522a2cca46ac5a8bfb
bahadirbagg <bahadirbagg@gmail.com>
2022-03-27 18:05:39 +0300

    initial commit

--- src/comonents/Header.jsx
+++ src/comonents/Header.jsx
@@ -8,17 +8,17 @@ function Header(){
                 <ul className="flex flex-col md:flex-row items-center">
                     <li>
                         <Link to="/">
-                            <p className="text-base sm:text-2xl md:text-4xl font-bold">Movie
+                            <p className="sm:text-3xl md:text-3xl lg:text-4xl font-bold">Movie
                                 <span className="text-red-600">Book</span>
                             </p>
                         </Link>    
                     </li>
-                    <li className="mt-3 md:ml-28 md:mt-0 border-2  border-gray-600  rounded-md " >
+                    <li className="mt-3 md:ml-28 md:mt-0 md:flex-col border-2  border-gray-600  rounded-md " >
                         <Link to="/MoviesOnCinema">
                             <p className="p-1">Movies On Cinema</p>
                         </Link>
                     </li>
-                    <li className="mt-3 mb-3 md:ml-5 md:mt-0 md:mb-0 border-2  border-gray-600  rounded-md ">
+                    <li className="mt-3 mb-3 md:ml-5 md:mt-0 md:mb-0 md:flex-col border-2  border-gray-600  rounded-md ">
                         <Link to="/UpcomingMovies">
                             <p className="p-1">Upcoming Movies</p>
                         </Link>
@@ -26,7 +26,7 @@ function Header(){
                     
 
                 </ul>
-                <div className="flex flex-col md:flex-row items-center">
+                <div className="flex flex-col md:flex-row md:ml-7 items-center">
                     <input type="text" className="bg-gray-800 rounded-full w-64 px-4 py-1 border-2 border-gray-600" placeholder="Search"/>
                 </div>
                
